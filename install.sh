eab7c069f37df9bdcf0f09612661597a977d873f
chmodmasx <daninicob@gmail.com>
2024-09-24 22:34:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cp /recursos/action/set-video-wallpaper.sh /$HOME/.local/share/nemo/actions/set-as-video-wallpaper-chmodmasx/
-cp /recursos/action/set-as-video-wallpaper.nemo_action /$HOME/.local/share/nemo/actions/
-sudo apt-get install mpv xorg-dev build-essential libx11-dev x11proto-xext-dev libxrender-dev libxext-dev xdotool -y
+sudo apt-get install mpv xorg-dev build-essential libx11-dev x11proto-xext-dev libxrender-dev libxext-dev xdotool zip unzip -y
 cd /recursos/xwinwrap
 make
 sudo make install
 make clean
-cd ..
+cd ../..
+cp /recursos/action/set-video-wallpaper.sh /$HOME/.local/share/nemo/actions/set-as-video-wallpaper-chmodmasx/
+cp /recursos/action/set-as-video-wallpaper.nemo_action /$HOME/.local/share/nemo/actions/
 
 mkdir -p "/$HOME/.local/share/nemo/actions/set-as-video-wallpaper-chmodmasx"
